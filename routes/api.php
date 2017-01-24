9501ba050b37da26cdd615b564d050cb87475659
Matt Wheatley <terrawheat@gmail.com>
2017-01-24 22:03:09 +0000

    Respond to OPTIONS requests universally

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/drafts', 'DraftController@index')->middleware('cors');
+Route::options('{all}', function () {
+          return response('OK', 200)
+                    ->header('Access-Control-Allow-Origin', '*')
+                    ->header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE')
+                    ->header('Access-Control-Allow-Headers', 'Content-Type, Origin, Authorization');
+});
